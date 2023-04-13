bddfd034e3bdea552500ccc2a8cb7c2e024d2312
KamilStypula <kamilstypula0510@gmail.com>
2023-04-13 12:09:26 +0200

    podstawa strony

--- impl/web/dvp-app/src/app/app-routing.module.ts
+++ impl/web/dvp-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'navbar', component: NavbarComponent }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- impl/web/dvp-app/src/app/app.component.ts
+++ impl/web/dvp-app/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dvp-app';
+  isDarkTheme: boolean = false;
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+  }
 }
