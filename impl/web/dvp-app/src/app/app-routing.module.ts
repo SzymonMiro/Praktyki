d36cb3258afb60b8bf57146d8258d8a1a8957f08
KamilStypula <kamilstypula0510@gmail.com>
2023-04-17 12:48:48 +0200

    routing

--- impl/web/dvp-app/src/app/app-routing.module.ts
+++ impl/web/dvp-app/src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './main/main.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { TablesComponent } from './tables/tables.component';
+
 
 const routes: Routes = [
-  { path: 'main', component: MainComponent },
-  { path: 'sidebar', component: SidebarComponent },
-  { path: 'navbar', component: NavbarComponent },
-  { path: 'tables', component: TablesComponent }
+
   
 ];
 
